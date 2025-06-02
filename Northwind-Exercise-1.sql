f12e62ebda83923ba7cd6b0d9e7d254a9850831a
Ethan.OToole <eotoole005@gmail.com>
2025-06-02 11:30:05 -0400

    Questions 1-10 answered

---
+++ Northwind-Exercise-1.sql
@@ -0,0 +1,32 @@
+/*1. What categories of products does Northwind Traders sell?
+-The categories are: Beverages, Condiments, Confections, Dairy Products, Grains/Cereals, Meat/Poultry, Produce, Seafood.
+2. What is the name of a product that sounds interesting to you?
+-"Jack's New England Clam Chowder" sounds the most interesting because I love any sort of chowder.
+3. What are some of the countries in which we have customers?
+-I am seeing customers from Germany, Mexico, UK, Sweden, France, Brazil, USA, Austria, Canada, Belgium. 
+4. What date was our first order placed? How did you determine that?
+-It was placed on: 1996-07-04 00:00:00. I selected from the orders table and clicked the sort arrow.
+5. What date was our last order placed?
+-Our date for our last order placed was: 1998-05-06 00:00:00. I selected from the orders table and clicked the sort arrow to go up instead of down.
+6. How many customers do we currently have?
+-Without using a SELECT COUNT query I counted out 93 by selected all from the customers table.
+7. Do we have any suppliers in South America? Europe? Africa?
+-We do have some suppliers in Europe like: Exotic Liquids(UK), PB Knckebrd AB(Sweden), Heli Swaren GmbH & Co. KG (Germany), and many more.
+-There are seems to be one supplier in South America: Refrescos Americanas LTDA(Brazil).
+-There does not seems to be any suppliers from Africa.
+8. What is our most expensive product?
+-Our most expensive product is: Cte de Blaye with a unit price of 263.50.
+9. What is our least expensive product?
+Our least expensive product is: Geitost with a unit price of 2.50.
+10. We have a customer named "The Big Cheese" if I wanted to get details
+about their last order from us how would you find that information?
+-We would need to select all from the orders table where the ship name is called "The Big Cheese"
+a. What tables would you use?
+1-9
+b. How would you link the data in the tables you are using
+c. What information might I want to know about their order?
+i. Order Date?
+ii. Sales Total?
+iii. Product Name?
+iv. Is there other information that you might find interesting?
+*/
